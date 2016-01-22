78088b607f3833bd6668f28f02ec4e19ce74e108
Johann Schmitz <johann@j-schmitz.net>
2016-01-22 06:53:44 +0100

    Highlight sidebar item in DashboardView

--- src/djunin/views/dashboard.py
+++ src/djunin/views/dashboard.py
@@ -6,4 +6,4 @@ from djunin.views.base import BaseViewMixin
 class DashboardView(BaseViewMixin, TemplateView):
 	page_title = _('Dashboard')
 	template_name = 'dashboard.html'
-
+	sidebar_item = 'dashboard'
