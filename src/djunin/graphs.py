086f0d06544d0d1a0ba2550ab51a0a8b72837f21
Johann Schmitz <johann@j-schmitz.net>
2016-01-24 07:58:11 +0100

    Use proper timestamp for correct time display

--- src/djunin/graphs.py
+++ src/djunin/graphs.py
@@ -5,10 +5,18 @@ import logging
 
 import rrdtool
 
+import datetime
 from django.conf import settings
 from django.core.urlresolvers import reverse
 
 logger = logging.getLogger(__file__)
+
+SCOPE_DAY = 1
+SCOPE_WEEK = 2
+SCOPE_MONTH = 3
+SCOPE_YEAR = 4
+SCOPE_RANGE = 5
+
 
 class GraphOptsGenerator(object):
 
@@ -58,18 +66,28 @@ class FlotGraphOptsGenerator(GraphOptsGenerator):
 
 class FlotGraphDataGenerator(GraphDataGenerator):
 
-	def generate(self, node, graph):
-
+	def generate(self, node, graph, data_scope=SCOPE_DAY):
 		def _gen():
+			date_range = ""
+			if data_scope == SCOPE_DAY:
+				date_range = "-s -28h"
+			elif data_scope == SCOPE_WEEK:
+				date_range = "-s -176h"
+			elif data_scope == SCOPE_MONTH:
+				date_range = "-s -756h"
+
 			for dr in graph.datarows.all():
-				(start, end, resolution), (no,), data = rrdtool.fetch([str(os.path.join(settings.MUNIN_DATA_DIR, dr.rrdfile)), 'AVERAGE'])
-				logger.debug("From %s to %s every %s sec", start, end, resolution)
+				(start, end, resolution), (no,), data = rrdtool.fetch([str(os.path.join(settings.MUNIN_DATA_DIR, dr.rrdfile)), 'AVERAGE', date_range])
+				logger.debug("%s/%s: from %s to %s every %s sec", graph.name, dr.name, datetime.datetime.fromtimestamp(start), datetime.datetime.fromtimestamp(end), resolution)
 
 				dr_opts = dict(((dro.key, dro.value) for dro in dr.options.all()))
 
 				flot_opts = {
 					'label': dr_opts.get('label', dr.name),
-					'data': zip(xrange(start, end, resolution), (x[0] for x in data)),
+					'data': zip(
+						(x * 1000 for x in xrange(start, end, resolution)),
+						(x[0] for x in data)
+					),
 				}
 
 				yield flot_opts
