c2d9230885beaaaa5a66233625c77d58d02b3085
Johann Schmitz <johann@j-schmitz.net>
2016-02-04 06:29:18 +0100

    Do not request data for invisible graphs

--- src/djunin/static/site.js
+++ src/djunin/static/site.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-	$('.djunin-graph').each(function(i, elem) {
+	$('.djunin-graph:visible').each(function(i, elem) {
 		var url = $(elem).data('url');
 		if (url) {
 			$.get(url, function(graph_data){
