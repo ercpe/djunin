30bf206e15e62079f4e14bcf3326a05cdbcf49ac
Johann Schmitz <johann@j-schmitz.net>
2016-02-24 06:37:43 +0100

    Improved search. Still kinda hackish

--- src/djunin/views/search.py
+++ src/djunin/views/search.py
@@ -18,7 +18,7 @@ class SearchView(View):
 		logger.debug("Suggestions for %s", query_string)
 
 		def _search():
-			chunks = [x.strip() for x in query_string.split() if x.strip()]
+			chunks = [x.strip().lower() for x in query_string.split() if x.strip()]
 
 			def _make_filter(field_name):
 				q = None
@@ -27,26 +27,41 @@ class SearchView(View):
 					q = f if q is None else q | f
 				return q
 
-			# groups
-			for group_name in Node.objects.filter(_make_filter('group')).values_list('group', flat=True).distinct():
-				yield {
-					'value': group_name,
-					'data': reverse('group_nodes', args=(group_name, )),
-				}
+			results = []
+
+			group_q = Node.objects.filter(_make_filter('group')).values_list('group', flat=True).distinct()
+			results.extend((
+				(group_name, reverse('group_nodes', args=(group_name, )), 2 if any((group_name.startswith(c) for c in chunks)) else 1)
+				for group_name in group_q
+			))
 
 			# nodes
-			for group_name, node_name in Node.objects.filter(_make_filter('name')).values_list('group', 'name').distinct():
-				yield {
-					'value': node_name,
-					'data': reverse('graphs', args=(group_name, node_name, )),
-				}
+			node_q = Node.objects.filter(_make_filter('name')).values_list('group', 'name').distinct()
+			results.extend((
+				(node_name, reverse('graphs', args=(group_name, node_name, )), sum([
+					1 if any((group_name.startswith(c) for c in chunks)) else 0,
+					1 if any((node_name.startswith(c) for c in chunks)) else 0,
+				]))
+				for group_name, node_name in node_q
+			))
 
 			# graph category
-			for group_name, node_name, graph_category in Graph.objects.filter(_make_filter('graph_category')).values_list('node__group', 'node__name', 'graph_category').distinct():
-				yield {
-					'value': "%s / %s" % (graph_category, node_name),
-					'data': reverse('graphs', args=(group_name, node_name, graph_category)),
-				}
+			graph_category_q = Graph.objects.filter(_make_filter('graph_category')).values_list('node__group', 'node__name', 'graph_category').distinct()
+			results.extend((
+				("%s / %s" % (graph_category, node_name), reverse('graphs', args=(group_name, node_name, graph_category)), sum([
+					1 if any((group_name.startswith(c) for c in chunks)) else 0,
+					1 if any((node_name.startswith(c) for c in chunks)) else 0,
+					1 if any((graph_category.startswith(c) for c in chunks)) else 0,
+				]))
+				for group_name, node_name, graph_category in graph_category_q
+			))
+
+			return [
+				{
+					'value': value,
+					'data': data
+				} for value, data, _ in sorted(results, key=lambda x: x[2], reverse=True)
+			]
 
 		d = {
 			'query': query_string,
