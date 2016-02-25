070fd1ec9e743a20d495b673b3e14a213e3109ec
Johann Schmitz <johann@j-schmitz.net>
2016-02-25 06:43:39 +0100

    Use next parameter to redirect after login

--- src/djunin/middleware.py
+++ src/djunin/middleware.py
@@ -30,4 +30,4 @@ work, ensure your TEMPLATE_CONTEXT_PROCESSORS setting includes\
 		if not request.user.is_authenticated():
 			path = request.path_info.lstrip('/')
 			if not any(m.match(path) for m in EXEMPT_URLS):
-				return HttpResponseRedirect(settings.LOGIN_URL)
+				return HttpResponseRedirect(settings.LOGIN_URL + '?next=%s' % request.path_info)
