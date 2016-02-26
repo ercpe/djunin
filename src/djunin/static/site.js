df3db94711dd1d1fa65e1d9cdc52874c3dd8fc0f
Johann Schmitz <johann@j-schmitz.net>
2016-02-26 06:33:06 +0100

    Added hover crosshairs for graphs to show the current values in the legend

--- src/djunin/static/site.js
+++ src/djunin/static/site.js
@@ -34,43 +34,111 @@ function align_legend(plot, offset) {
 	});
 }
 
-$(document).ready(function () {
+var updateLegendTimeout = null;
+var latestPosition = null;
+
+
+function draw_graphs() {
 	$('.djunin-graph:visible').each(function(i, elem) {
 		var url = $(elem).data('url');
-		if (url) {
-			$.get(url, function(graph_data){
+		if (!url) return;
 
-				if (graph_data['datarows'].length == 0) {
-					$(elem).html('<div class="alert alert-info">This graph has no data.</div>');
-					$(elem).css('height', 'auto');
+		$.get(url, function(graph_data){
+			var opts = graph_data['options'];
+			var meta = graph_data['_meta'];
+			var legend_container = $('#graph-' + graph_data['graph_name'] + "-" + meta['scope'] + "-legend");
+
+			if (graph_data['datarows'].length == 0) {
+				$(elem).html('<div class="alert alert-info">This graph has no data.</div>');
+				$(elem).css('height', 'auto');
+				return;
+			}
+
+			opts['legend'] = { container: legend_container }
+
+			if (meta['autoscale'] === true) {
+				var base = meta['base'] || 1000;
+				opts['yaxis']['tickFormatter'] = function(val, axis) {
+					return suffixFormatter(base, val, axis);
+				}
+			}
+
+			opts['crosshair'] = { mode: 'x', };
+			opts['grid'] = $.extend(opts['grid'], {
+				hoverable: true,
+				autoHighlight: false
+			});
+
+			opts['hooks'] = {
+				processOffset: align_legend,
+			}
+
+			var plot = $.plot($(elem), graph_data['datarows'], opts);
+
+			// custom stuff for our graphs
+			$('table', legend_container).css('width', '100%');
+			$('table', legend_container).prepend('<tr><th colspan="2"></th><th>Min</th><th>Max</th><th>Current</th></tr>');
+			$('.legendLabel', legend_container).addClass('col-md-6')
+			$('tr', legend_container).append('<td class="datarow-min col-md-2"></td><td class="datarow-max col-md-2"></td><td class="datarow-current col-md-2"></td>');
+
+
+			function update_legend() {
+				updateLegendTimeout = null;
+
+				var pos = latestPosition;
+
+				var axes = plot.getAxes();
+				if (pos.x < axes.xaxis.min || pos.x > axes.xaxis.max ||
+					pos.y < axes.yaxis.min || pos.y > axes.yaxis.max) {
 					return;
 				}
 
-				var opts = graph_data['options'];
-				var meta = graph_data['_meta'];
+				var i, j, dataset = plot.getData();
+				for (i = 0; i < dataset.length; ++i) {
+					var series = dataset[i];
 
-				opts['legend'] = {
-					container: $('#graph-' + graph_data['graph_name'] + "-" + meta['scope'] + "-legend")
-				}
-
-				if (meta['autoscale'] === true) {
-					var base = meta['base'] || 1000;
-					opts['yaxis']['tickFormatter'] = function(val, axis) {
-						return suffixFormatter(base, val, axis);
+					// Find the nearest points, x-wise
+					for (j = 0; j < series.data.length; ++j) {
+						if (series.data[j][0] > pos.x) {
+							break;
+						}
 					}
-				}
 
-				opts['hooks'] = {
-					processOffset: align_legend
-				}
+					// Now Interpolate
+					var y,
+						p1 = series.data[j - 1],
+						p2 = series.data[j];
 
-				$.plot($(elem), graph_data['datarows'], opts);
-			}).fail(function() {
-				$(elem).html('<div class="alert alert-danger">There was a error fetching the data for this graph :(</div>');
-				$(elem).css('height', 'auto');
+					if (p1 == null) {
+						y = p2[1];
+					} else if (p2 == null) {
+						y = p1[1];
+					} else {
+						y = p1[1] + (p2[1] - p1[1]) * (pos.x - p1[0]) / (p2[0] - p1[0]);
+					}
+
+					$('tr:nth-child(' + (i+2) + ') > .datarow-current', legend_container).text(series.yaxis.tickFormatter(y, series.yaxis));
+				}
+			}
+
+
+			$(elem).bind("plothover",  function (event, pos, item) {
+				latestPosition = pos;
+				if (!updateLegendTimeout) {
+					updateLegendTimeout = setTimeout(update_legend, 50);
+				}
 			});
-		}
+
+		}).fail(function() {
+			$(elem).html('<div class="alert alert-danger">There was a error fetching the data for this graph.</div>');
+			$(elem).css('height', 'auto');
+		});
 	});
+}
+
+$(document).ready(function () {
+
+	draw_graphs();
 
 	$('#site-search').autocomplete({
 		serviceUrl: $('#site-search').data('url'),
