9cf049736917198f66e39144d262608f95913340
Johann Schmitz <johann@j-schmitz.net>
2016-08-18 06:20:22 +0200

    removed brand_url and brand_title to be able to click back to the graphs from management views

--- djunin/views/management.py
+++ djunin/views/management.py
@@ -14,43 +14,41 @@ from django.contrib import messages
 
 
 class ManagementBaseViewMixin(BaseViewMixin):
-	brand_title = _('Management')
-	brand_url = reverse_lazy('manage')
 
-	@method_decorator(staff_member_required)
-	def dispatch(self, request, *args, **kwargs):
-		return super(ManagementBaseViewMixin, self).dispatch(request, *args, **kwargs)
+    @method_decorator(staff_member_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(ManagementBaseViewMixin, self).dispatch(request, *args, **kwargs)
 
 
 class ManagementIndexView(ManagementBaseViewMixin, TemplateView):
-	template_name = 'djunin/management/index.html'
-	page_title = _('Management')
+    template_name = 'djunin/management/index.html'
+    page_title = _('Management')
 
 
 class ManagementUpdateView(ManagementBaseViewMixin, FormView):
-	template_name = 'djunin/management/update.html'
-	sidebar_item = 'management_update'
-	page_title = _('Management')
-	form_class = ManagementUpdateForm
-	success_url = reverse_lazy('management_update')
+    template_name = 'djunin/management/update.html'
+    sidebar_item = 'management_update'
+    page_title = _('Management')
+    form_class = ManagementUpdateForm
+    success_url = reverse_lazy('management_update')
 
-	@method_decorator(staff_member_required)
-	def dispatch(self, request, *args, **kwargs):
-		return super(ManagementUpdateView, self).dispatch(request, *args, **kwargs)
+    @method_decorator(staff_member_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(ManagementUpdateView, self).dispatch(request, *args, **kwargs)
 
-	def form_valid(self, form):
-		Updater().run()
-		messages.add_message(self.request, messages.INFO, 'Nodes and graphs successfully updated')
-		return super(ManagementUpdateView, self).form_valid(form)
+    def form_valid(self, form):
+        Updater().run()
+        messages.add_message(self.request, messages.INFO, 'Nodes and graphs successfully updated')
+        return super(ManagementUpdateView, self).form_valid(form)
 
 
 class ManagementPermissionsView(ManagementBaseViewMixin, FormView):
-	template_name = 'djunin/management/permissions.html'
-	sidebar_item = 'management_permissions'
-	page_title = _('Permissions')
-	form_class = ManagementUpdateForm
-	success_url = reverse_lazy('management_permissions')
+    template_name = 'djunin/management/permissions.html'
+    sidebar_item = 'management_permissions'
+    page_title = _('Permissions')
+    form_class = ManagementUpdateForm
+    success_url = reverse_lazy('management_permissions')
 
-	def get_context_data(self, **kwargs):
-		kwargs.setdefault('nodes', Node.objects.for_user(self.request.user).all())
-		return super(ManagementPermissionsView, self).get_context_data(**kwargs)
+    def get_context_data(self, **kwargs):
+        kwargs.setdefault('nodes', Node.objects.for_user(self.request.user).all())
+        return super(ManagementPermissionsView, self).get_context_data(**kwargs)
